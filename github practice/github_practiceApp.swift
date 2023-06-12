75d7273b0d084940d1b7cc570afc884d1bea0bd9
helloish <ishunamburu@gmail.com>
2023-06-12 08:02:26 -0700

    Initial Commit

---
+++ github practice/github_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_practiceApp.swift
+//  github practice
+//
+//  Created by Sravya Namburu on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct github_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
